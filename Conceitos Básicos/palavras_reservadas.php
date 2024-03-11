3be4e87b5033f19868b57c58d306e237bb4761fb
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2024-03-11 15:12:23 -0300

    Explicação sobre palavras reservadas do PHP

---
+++ Conceitos Básicos/palavras_reservadas.php
@@ -0,0 +1,12 @@
+<?php
+    // function echo(){ Erro por que echo é uma palavra reservada do PHP
+    //     return true; 
+    // }
+
+    // function while(){ Erro por que while é uma palavra reservada do PHP
+    //     return true;
+    // }
+
+    function echoNew(){
+        return true;
+    }
