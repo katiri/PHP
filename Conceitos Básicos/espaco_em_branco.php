fde356db9321fbbec21602be70a75c952c10f116
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2024-03-11 15:01:17 -0300

    Explicação sobre como o PHP trata espaços em branco no código

---
+++ Conceitos Básicos/espaco_em_branco.php
@@ -0,0 +1,12 @@
+<?php
+    
+    // Espaços em braco são ignorados pelo PHP
+    echo                              "Teste 1 <br>";
+    echo "Teste 2 <br>"                      ;
+    echo
+    "Teste 3 <br>"
+    ;
+    echo
+    "Teste 
+    4 <br>"
+    ;
