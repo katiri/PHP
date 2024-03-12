d0fff54cc982daf5ecd08dae8d5917774f6a45b6
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2024-03-12 14:28:02 -0300

    Explicação de como se declara variáveis, como se atribiu valores a elas e qual o melhor formato para nomear elas

---
+++ variaveis/declarando_variaveis.php
@@ -0,0 +1,14 @@
+<?php
+    $texto = 'Texto qualquer';
+    $numero = 5; // Número qualquer
+    $_variavel = 0.3;
+
+    $somaDeVariaveis = $numero + $_variavel;
+
+    $nomeVariaveis = 'Toda variável deve começar com $ seguida de seu nome, o formato mais recomendável para o nome de uma variável 
+    é o nome sem carcteres especiais, no caso de mais de uma palavra, começar com primeira palavra toda minuscula e a primeira letra 
+    das outras palavras em maiúsculo, como o nome dessa variável = $nomeVariaveis';
+
+    echo $nomeVariaveis;
+
+    
