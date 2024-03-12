b6066f29dbcf520144a2a31062d1b9353a089638
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2024-03-12 10:15:14 -0300

    Simplificação do nome da pasta Conceitos Básicos para conceitos_basicos para manter as boas práticas

---
+++ conceitos_basicos/case_sensitive.php
@@ -0,0 +1,13 @@
+<?php
+    
+    // Não é Case Sensitive
+    echo "Teste <br>";
+    EcHo "Teste 2 <br>";
+
+    // É Case Sensitive
+    $nome = 'João Pedro';
+    $NOME = 'João Pedro Pazos Ramos';
+
+    echo $nome;
+    echo "<br>";
+    echo $NOME;
---
+++ conceitos_basicos/exercicio.php
@@ -0,0 +1,24 @@
+<?php
+    // Objeto: Garrafa
+
+    /* 
+    Características:
+    Volume
+    Cor
+    Material
+    */
+
+    $objeto = 'Garrafa';
+
+    $volume = '1L';
+    $cor = 'Azul';
+    $material = 'Metal';
+
+    echo $objeto;
+    echo '<br><br>';
+    echo $volume;
+    echo '<br>';
+    echo $cor;
+    echo '<br>';
+    echo $material;
+    echo '<br>';
