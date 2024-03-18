adc7b435145c5d355a44c6a0431190a4e0368e31
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2024-03-18 16:54:42 -0300

    Explicação sobre o operador de concatenação

---
+++ operadores/operador_concatanacao.php
@@ -0,0 +1,11 @@
+<?php
+    // Server para juntar valores em strings
+    $a = 'Teste';
+    $b = 'Aplicação';
+    $c = $a . ' ' . $b;
+    echo "$c <br>";
+
+    $a = 5;
+    $b = 5;
+    $c = $a . $b;
+    echo "$c <br>";
