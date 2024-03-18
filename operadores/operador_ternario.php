43279f95cd7373af1c0a85623d9c728f194fafdb
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2024-03-18 17:08:53 -0300

    Explicação sobre operador ternário

---
+++ operadores/operador_ternario.php
@@ -0,0 +1,11 @@
+<?php
+    // É como se fosse um if/else resumido
+    // Estrutura:
+    // (comparação(ões)) ? Valor se verdadeiro : Valor se falso
+    $a = 5;
+    $b = 2;
+    echo $a > $b ? "$a é maior que $b <br>" : "$a não é maior que $b <br>";
+
+    $a = 5;
+    $b = 7;
+    echo $a > $b ? "$a é maior que $b <br>" : "$a não é maior que $b <br>";
