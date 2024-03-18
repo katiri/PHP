39688c1391eb1d9e07494c4d2a029e5c2eb53ef8
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2024-03-18 15:56:57 -0300

    Explicação sobre operadores de incremento e decremento

---
+++ operadores/operadores_incremento.php
@@ -0,0 +1,28 @@
+<?php
+    // Operador de pós-incremento
+    // Em uma expressão retorna o valor da variavel e incrementa depois, ou seja o incremento só vale para expressões futuras
+    $a = 5;
+    echo $a++;
+    echo "<br>";
+    echo $a;
+    echo "<br>";
+
+    // Operador de pré-incremento
+    // Em uma expressão retorna o valor incrementado da variável
+    $a = 5;
+    echo ++$a;
+    echo "<br>";
+
+    // Operador de pós-decremento
+    // Em uma expressão retorna o valor da variavel e decrementa depois, ou seja o decremento só vale para expressões futuras
+    $a = 5;
+    echo $a--;
+    echo "<br>";
+    echo $a;
+    echo "<br>";
+
+    // Operador de pré-decremento
+    // Em uma expressão retorna o valor decrementado da variável
+    $a = 5;
+    echo --$a;
+    echo "<br>";
