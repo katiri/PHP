f65dd5ec1ec0cbf30b11c07b317f20788aca49d6
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2024-03-18 17:03:25 -0300

    Explicação sobre os operadores de atribuição

---
+++ operadores/operadores_atribuicao.php
@@ -0,0 +1,53 @@
+<?php
+    // Operador de atribuição base =
+    $a = 10;
+    echo "$a <br>";
+
+    // Operador de atribuição de adição
+    // $a += $b é o equivalente a $a = $a + $b
+    $a = 1;
+    $b = 2;
+    $a += $b;
+    echo "$a <br>";
+
+    // Operador de atribuição de subtração
+    // $a -= $b é o equivalente a $a = $a - $b
+    $a = 1;
+    $b = 2;
+    $a -= $b;
+    echo "$a <br>";
+
+    // Operador de atribuição de multiplicação
+    // $a *= $b é o equivalente a $a = $a * $b
+    $a = 1;
+    $b = 2;
+    $a *= $b;
+    echo "$a <br>";
+
+    // Operador de atribuição de divisão
+    // $a /= $b é o equivalente a $a = $a / $b
+    $a = 1;
+    $b = 2;
+    $a /= $b;
+    echo "$a <br>";
+
+    // Operador de atribuição de modulo
+    // $a %= $b é o equivalente a $a = $a % $b
+    $a = 1;
+    $b = 2;
+    $a %= $b;
+    echo "$a <br>";
+
+    // Operador de atribuição de exponenciação
+    // $a **= $b é o equivalente a $a = $a ** $b
+    $a = 1;
+    $b = 2;
+    $a **= $b;
+    echo "$a <br>";
+    
+    // Operador de atribuição de concatenação
+    // $a /= $b é o equivalente a $a = $a / $b
+    $a = 'Teste';
+    $b = ' da aplicação';
+    $a .= $b;
+    echo "$a <br>";
