9ba01d0b0dfc497bbbfb656c17439961fd285872
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2024-03-19 14:18:37 -0300

    Explicação sobre a estrutura de repetição foreach

---
+++ estruturas_de_repeticao/foreach.php
@@ -0,0 +1,8 @@
+<?php
+    // Funciona como um laço de repetição orientado a um Array
+
+    $nomes = ['João', 'Diogo', 'Catharina', 'José', 'Fernanda'];
+
+    foreach($nomes as $nome){
+        echo "O nome do índice atual é $nome <br>";
+    }
