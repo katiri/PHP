a69694ad9f1e5b32edb157f341f5af0864e1de54
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2024-03-20 09:59:23 -0300

    Explicação sobre como chamar uma função

--- funcoes/estrutura.php
+++ funcoes/estrutura.php
@@ -3,7 +3,13 @@
     // Isso serve para que eu possa executar operações complexas várias vezes sem precisar codificar elas todas as vezes
 
     // Declarando uma função:
-
     function nomeFuncao(){
         // Código da função
+        echo "Texto dentro da função <br>";
     }
+
+    
+    // Chamando uma função
+    nomeFuncao();
+    
+    nomeFuncao();
