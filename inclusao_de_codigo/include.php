cd77fcb312f74e9c4f2cc6742ae299b373a70521
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2024-03-20 08:28:17 -0300

    Explicação sobre o método de inclusão de código include

---
+++ inclusao_de_codigo/include.php
@@ -0,0 +1,9 @@
+<?php
+    include "teste.php";
+?>
+
+<p>Após o include</p>
+
+<p>Se o arquivo não existir, usar o método include não gera erro fatal, gera apenas um warning, que pode ser desabilitado, ocultando o erro</p>
+
+<p>Include pode incluir tanto arquivos .html, como arquivos .php</p>
---
+++ inclusao_de_codigo/teste.php
@@ -0,0 +1,7 @@
+<?php
+    $c = 5
+?>
+
+<h1>Olá mundo</h1>
+
+<p>Exibindo $c=<?php echo $c; ?></p>
