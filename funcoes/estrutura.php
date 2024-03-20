63252765482fbf6dc0b290ac662a29621441e81d
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2024-03-20 09:49:55 -0300

    Explicação sobre a estrutura básica de uma função

---
+++ funcoes/estrutura.php
@@ -0,0 +1,9 @@
+<?php
+    // Funções são blocos de código com nome próprio que pode ser utilizados várias vezes no código
+    // Isso serve para que eu possa executar operações complexas várias vezes sem precisar codificar elas todas as vezes
+
+    // Declarando uma função:
+
+    function nomeFuncao(){
+        // Código da função
+    }
