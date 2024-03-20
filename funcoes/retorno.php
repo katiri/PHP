9ee421bc28ea85a2215e565fd0eb446e0a4ecc62
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2024-03-20 11:24:32 -0300

    Explicação sobre como uma função pode retornar vários valores

--- funcoes/retorno.php
+++ funcoes/retorno.php
@@ -18,3 +18,21 @@
     echo "<br>";
 
     echo soma($s, 100);
+    echo "<br>";
+
+
+    // Retornando vários valores
+    function randomPessoa(){
+        $nomes = ['João', 'Diogo', 'Fernanda', 'José', 'Catharina', 'Thiago', 'Beatriz', 'Pedro'];
+
+        $nome = $nomes[rand(0, count($nomes) - 1)];
+
+        $idade = rand(0, 60);
+        $altura = rand(0, 2);
+
+        return [$nome, $idade, $altura, 'Humano'];
+    }
+
+    $pessoa = randomPessoa();
+
+    echo $pessoa[0] . " tem " . $pessoa[1] . " anos de idade, " . $pessoa[2] . " m de altura e é " . $pessoa[3] . "<br>";
