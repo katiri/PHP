97dba785c647c07a99824d020f15d0df5dc01a10
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2024-03-20 08:59:07 -0300

    Explicação sobre os métodos de inclusão de código include_onde e require_once

--- inclusao_de_codigo/include.php
+++ inclusao_de_codigo/include.php
@@ -7,3 +7,9 @@
 <p>Se o arquivo não existir, usar o método include não gera erro fatal, gera apenas um warning, que pode ser desabilitado, ocultando o erro</p>
 
 <p>Include pode incluir tanto arquivos .html, como arquivos .php</p>
+
+<?php
+    include_once "once.html";
+    
+    include_once "once.html";
+?>
--- inclusao_de_codigo/require.php
+++ inclusao_de_codigo/require.php
@@ -7,3 +7,9 @@
 <p>Se o arquivo não existir, usar o método require gera erro fatal, parando a aplicação</p>
 
 <p>Require pode incluir tanto arquivos .html, como arquivos .php</p>
+
+<?php
+    require_once "once.html";
+
+    require_once "once.html";
+?>
