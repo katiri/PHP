f86289967427a47ec4361c476c134711dbf0870d
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2024-03-20 08:33:04 -0300

    Explicação sobre o método de inclusão de código require

---
+++ inclusao_de_codigo/require.php
@@ -0,0 +1,9 @@
+<?php
+    require "teste.php";
+?>
+
+<p>Após o require</p>
+
+<p>Se o arquivo não existir, usar o método require gera erro fatal, parando a aplicação</p>
+
+<p>Require pode incluir tanto arquivos .html, como arquivos .php</p>
