c623c692a29d194b2fe347b748927ff63321d16e
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2024-03-21 08:33:45 -0300

    Explicação da função printf

---
+++ strings/printf.php
@@ -0,0 +1,9 @@
+<?php
+    // Parecida com a função print mas é possível interpolar variáveis com o simbolo %, como em C++ por exemplo
+
+    // %d para inteiros
+    // %f para float
+    // %.2f para float com duas casas depois da virgula
+    // %s para strings
+
+    printf("Valores: %d %f %.2f %s", 1, 14.58, 54.43141412, 'String');
