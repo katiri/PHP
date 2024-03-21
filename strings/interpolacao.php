2ecbc2ad015a80e5259b50954c34b620c116ae62
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2024-03-21 08:12:55 -0300

    Explicação sobre interpolação de variáveis em strings

---
+++ strings/interpolacao.php
@@ -0,0 +1,10 @@
+<?php
+    $nome = 'João';
+    $idade = 22;
+    $profissao = 'Desenvolvedor';
+
+    echo "Eu sou o $nome, tenho $idade anos e sou $profissao <br>";
+
+    echo "Eu sou o {$nome}, tenho {$idade} anos e sou {$profissao} <br>";
+
+    // Não há diferença entre as duas formas
