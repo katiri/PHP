b38a919d016338d289b0b5553a86716c50d3bf52
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2024-03-21 08:36:51 -0300

    Explicação da função strlen

---
+++ strings/tamanho_string.php
@@ -0,0 +1,8 @@
+<?php
+    // Para encontrar o tamanho de caracteres de uma string se usa a função strlen()
+
+    $str1 = 'Uma string bem grande e espaçosa';
+    $str2 = 'String menor';
+
+    echo strlen($str1) . '<br>';
+    echo strlen($str2) . '<br>';
