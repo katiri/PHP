5f5af642590b077c0c245454105624106b6d5b79
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2024-03-22 11:59:14 -0300

    Explicação de como usar o foreach com array associativo

--- estruturas_de_repeticao/foreach.php
+++ estruturas_de_repeticao/foreach.php
@@ -6,3 +6,17 @@
     foreach($nomes as $nome){
         echo "O nome do índice atual é $nome <br>";
     }
+
+    // Array associativo
+
+    $arrAssoc = [
+        'marca' => 'BMW',
+        'motor' => '2.4',
+        'teto_solar' => true,
+        'cambio' => 'manual',
+        'portas' => 4,
+    ];
+
+    foreach($arrAssoc as $key => $value){
+        echo "$key - $value <br>";
+    }
