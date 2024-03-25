3488ba6019bdb630cde873597ec25ed64322abf2
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2024-03-25 08:36:58 -0300

    Explicação básica sobre o que são objetos, classes, propriedades e métodos e como criar uma nova classe

---
+++ orientacao_a_objetos/o_que_sao_objetos.php
@@ -0,0 +1,11 @@
+<?php
+    // Objetos são basicamente entidades que possuem comportamentos e características
+    // Em PHP objetos são definidos por classes, seus comportamentos são funções próprias e suas caracteríticas, variáveis próprias
+    // Essas funções também podem ser chamadas de métodos e as variáveis podem ser chamadas de propriedades
+    // As classes contém o molde do objetos, com seus métodos e propriedades iniciais
+    // Os valores das propriedades podem ser mudados ao invocar um novo objeto daquela classe, mas esses valores partem do que a classe impõe
+
+    // Declarando uma classe
+    class User{
+        // Aqui serão declaradas as propriedades e métodos
+    }
