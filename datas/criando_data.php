d7bccb99584ad98a44fe37e7637104554c24c499
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2024-03-26 08:24:10 -0300

    Explicação sobre como criar datas no PHP com a função date

---
+++ datas/criando_data.php
@@ -0,0 +1,7 @@
+<?php
+    echo date('12 - 01 - 2023') . "<br>"; // Dia / Mês / Ano
+    echo date('d/m/Y') . "<br>";
+    echo date('d, M - Y') . "<br>";
+
+    // https://www.php.net/manual/en/function.date.php
+    // Manual sobre como usar date na documentação do PHP com todas as siglas que podem ser usadas no parâmetro
