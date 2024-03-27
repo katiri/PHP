be87e112b482ffd8089809abc764c39df17ac90b
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2024-03-27 08:43:16 -0300

    Explicação sobre o que é a variável global $_SERVER do PHP

---
+++ ferramentas_web/server.php
@@ -0,0 +1,7 @@
+<?php
+    // $_SERVER é uma variável global do PHP que armazene em forma de Array informações sobre o servidor e a página de acesso atual
+
+    print_r($_SERVER);
+    echo "<br><br>";
+
+    echo $_SERVER['SERVER_SOFTWARE'] . "<br>";
